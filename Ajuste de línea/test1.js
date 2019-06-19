e97f79002ae5e371248bea7ae638981793d251ab
Ivan Hernandez <44682635+Ivanherd@users.noreply.github.com>
2019-06-18 21:57:36 -0400

    Add files via upload

---
+++ Ajuste de línea/test1.js
@@ -0,0 +1,14 @@
+
+const text = prompt();
+
+var number = Number(prompt());
+
+function wordWrap(text, number){
+    
+    return {text, number}
+
+}
+
+const result = wordWrap(text, mumber);
+
+console.log(result);
